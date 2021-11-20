8025979b49876ddad478e4b1cb8f1641b1527488
Dylan Pee <peedylan94@gmail.com>
2021-11-20 13:44:10 +1030

    completed models rspec test

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, length: { minimum: 6, message: 'Comment cannot be shorter than 6 characters' }
+  validates :movie_id, uniqueness: { scope: :list_id }
 end
